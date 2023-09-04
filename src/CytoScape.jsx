4d49eab7d0f388596dc0b7c628f337a7651755cf
DevvSakib <devvsakib@gmail.com>
2023-09-04 12:19:42 +0600

    tree view

--- src/CytoScape.jsx
+++ src/CytoScape.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Graph from './Cytoscape/Graph'
+import TreeView from './Cytoscape/TreeView'
 
 const App = () => {
   return (
     <div>
         <h1 className='py-2 text-center'>Cytoscape</h1>
         <Graph />
+        <TreeView />
     </div>
   )
 }
