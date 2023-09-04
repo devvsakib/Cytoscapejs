a7c981c3d34f511bfc72e295988dac2c0e7c9bad
DevvSakib <devvsakib@gmail.com>
2023-09-04 12:01:40 +0600

    changes

--- src/Cytoscape/Graph.jsx
+++ src/Cytoscape/Graph.jsx
@@ -15,7 +15,7 @@ const Graph = () => {
     const cy = useRef(null);
     const [selectedNodeLabel, setSelectedNodeLabel] = useState('');
     const [secondSelect, setSecondSelect] = useState('');
-
+    const [currentParentNode, setCurrentParentNode] = useState('Internet');
     useEffect(() => {
         // Initialize Cytoscape
         cy.current = cytoscape({
@@ -27,7 +27,6 @@ const Graph = () => {
                 edges: [],
             },
             style: [
-                // Styling for nodes and edges (you can customize this)
                 {
                     selector: 'node',
                     style: {
@@ -35,7 +34,7 @@ const Graph = () => {
                         height: 15,
                         label: 'data(label)',
                         'background-opacity': 0,
-                        "font-size": "10px",
+                        'font-size': '10px',
                         'background-image': node => node.data('background-image') || "url('https://js.cytoscape.org/img/cytoscape-logo.png')",
                         'background-size': 'contain',
                         'background-fit': 'cover',
@@ -53,48 +52,43 @@ const Graph = () => {
                     },
                 },
             ],
-            zoom: .2,
+            zoom: 0.2,
             minZoom: 1,
             maxZoom: 3,
-            wheelSensitivity: .2,
+            wheelSensitivity: 0.2,
         });
 
         cy.current.on('click', 'node', (event) => {
             const node = event.target;
             node.addClass('faded');
-            console.log(node.json());
+            console.log(node.roots([currentParentNode]));
         });
 
+        cy.current.layout({ name: 'cose' });
 
-        cy.current.layout({
-            name: 'preset',
-            spacingFactor: 0.2,
-            nodeSpacing: 20,
-        }).run();
     }, []);
 
-    const handleAddNode = (v, option) => {
-        option === "second" ? setSecondSelect(v) : setSelectedNodeLabel(v)
-
-
+    const handleAddNode = (value, parentNode) => {
+        value == "B" && setSelectedNodeLabel(value);
         if (!cy.current) return;
+
         // Check if a node label is valid and already selected
-        const isValidLabel = v && optionImages[v];
+        const isValidLabel = value && optionImages[value];
 
         if (isValidLabel) {
             const newNodeId = `n${cy.current.nodes().length}`;
-            const selectedOptionImageUrl = optionImages[v]; // Calculate the image URL here
+            const selectedOptionImageUrl = optionImages[value];
             const newNode = {
                 data: {
                     id: newNodeId,
-                    label: v,
+                    label: value,
                     backgroundColor: 'transparent',
                     'background-image': selectedOptionImageUrl,
                     'background-fit': 'cover',
                 },
             };
             cy.current.add(newNode);
-            cy.current.add({ data: { source: 'Internet', target: newNodeId } });
+            cy.current.add({ data: { source: parentNode, target: newNodeId } });
             cy.current.layout({
                 name: 'cose',
                 spacingFactor: 0.2,
@@ -109,25 +103,23 @@ const Graph = () => {
                 <select
                     className='px-2 py-1 border border-gray-300 rounded-md'
                     value={selectedNodeLabel}
-                    onChange={(e) => handleAddNode(e.target.value)}
+                    onChange={(e) => handleAddNode(e.target.value, "Internet")}
                 >
                     <option value=''>Select a node label</option>
                     <option value='A'>Vite</option>
                     <option value='B'>React</option>
                 </select>
-                {
-                    selectedNodeLabel == "B" &&
+                {selectedNodeLabel === "B" && (
                     <select
                         className='px-2 py-1 border border-gray-300 rounded-md'
                         value={secondSelect}
-                        onChange={(e) => handleAddNode(e.target.value, "second")}
+                        onChange={(e) => handleAddNode(e.target.value, "B")}
                     >
                         <option value=''>Select a node label</option>
                         <option value='F'>F</option>
                         <option value='T'>T</option>
                     </select>
-                }
-                {/* <button onClick={handleAddNode} className=''>Add Node</button> */}
+                )}
             </div>
             <div className='mx-2 h-[80vh]' ref={ref}></div>
         </div>
